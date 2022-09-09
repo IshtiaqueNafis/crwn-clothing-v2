2a9e9304205e28d53c577fd3cc14b17392e9faba
nafis ishtiaque <nafu19@hotmail.com>
2022-09-08 20:57:44 -0700

    solved issue with addding and removing item from the cart

--- src/redux/reducer/cartSliceReducer.js
+++ src/redux/reducer/cartSliceReducer.js
@@ -15,25 +15,45 @@ export const cartSlice = createSlice({
             state.isCartOpen = !state.isCartOpen;
 
         },
-        addCartItem: (state, {payload: {product}}) => {
+        addCartItem: (state, {payload: {product, quantity = 1}}) => {
 
-            const existingCartItem = state.cartItems.find((cartItem) => (
+            const existingCartItemIndex = state.cartItems.findIndex((cartItem) => (
                     cartItem.id === product.id
                 )
             );
 
-            if (existingCartItem) {
+            if (existingCartItemIndex !== -1) {
 
-                state.cartItems = state.cartItems.map(cartItem => cartItem.id === product.id ? {
-                        ...cartItem, quantity: cartItem.quantity + 1
-                    } : cartItem
-                );
+                if (quantity === -1) {
+                    state.cartItems = state.cartItems.map(cartItem => cartItem.id === product.id ? {
+                            ...cartItem, quantity: cartItem.quantity - 1
+                        } : cartItem
+                    ).map(cart => cart.quantity <= 0 ? {} : {...cart})
+                        .filter(element => {
+                                if (Object.keys(element).length !== 0) {
+                                    return true;
+                                }
 
+                                return false;
+                            }
+                        )
+
+
+                } else if (quantity === 0) {
+                    state.cartItems = state.cartItems.filter(cart => cart.id !== product.id);
+                } else {
+                    state.cartItems = state.cartItems.map(cartItem => cartItem.id === product.id ? {
+                            ...cartItem, quantity: cartItem.quantity + 1
+                        } : cartItem
+                    );
+                }
 
             } else {
 
                 state.cartItems = [...state.cartItems, {...product, quantity: 1}];
             }
+
+
             state.cartCount = state.cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
             state.cartTotal = state.cartItems.map(cart => (cart.price * cart.quantity)).reduce((result, item) => result + item, 0);
 
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,20 +1,34 @@
-import React from 'react';
-import {useSelector} from "react-redux";
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {addCartItem} from "../../redux/reducer/cartSliceReducer";
 
 const CheckOut = () => {
-    const {cartItems} = useSelector(state => state.cartState);
+    const {cartItems,cartTotal} = useSelector(state => state.cartState);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+
+    }, [cartTotal]);
 
     return (
         <div>
             {cartItems.map(cart => (
-                <div key={cart.id}>
+
+                <div key={cart.id} >
                     <h1>{cart.name}</h1>
                     <h2>{cart.price}</h2>
                     <h2>{cart.quantity}</h2>
+                    <button onClick={()=>{dispatch(addCartItem({product:cart}))}}>INCREASE</button>
+                    <button onClick={()=>{dispatch(addCartItem({product:cart,quantity:-1}))}}>DECREASE</button>
+                    <button onClick={()=>{dispatch(addCartItem({product:cart,quantity:0}))}}>Remove Item</button>
+                    <h3>------------------------------------------</h3>
                 </div>
 
 
             ))}
+            <div>
+                total :{cartTotal}
+            </div>
         </div>
     );
 };
